ec32e6ccf9a348dc8424d836523aea03e18d6d9f
Danny <dannybrudner@gmail.com>
2019-04-11 04:59:10 -0400

    Completed skeletons of all routes.

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,12 +11,41 @@ router.get('/sanity', function (req, res) {
 })
 
 
-router.get('/weather/:city', function(req, res) {
-    let city = req.params.city
-    request(`https://api.apixu.com/v1/current.json?key=${API_KEY}&q=${city}`, function(err, response) {
+router.get('/city/:cityName', function(req, res) {
+
+    //ROUTE THAT MAKES CALLS TO API
+
+    let cityName = req.params.cityName
+    request(`https://api.apixu.com/v1/current.json?key=${API_KEY}&q=${cityName}`, function(err, response) {
         let currentWeatherObj = JSON.parse(response.body)
         res.send(currentWeatherObj)
     })
+})
+
+router.get('/cities', function(req, res) {
+    //ROUTE THAT SAVES ALL CITY DATA SAVED IN DB AND SENDS IT TO CLIENT
+})
+
+router.post('/city', function(req, res) {
+    //TAKE DATA FROM BODY OF REQUEST AND SAVE AS A NEW CITY TO DATABASE
+    let reqCity = req.body
+
+    let newCity = new City({
+        name: reqCity.name,
+        updatedAt: reqCity.updatedAt,
+        temperature: reqCity.temperature,
+        condition: reqCity.condition,
+        conditionPic: reqCity.conditionPic
+    })
+
+    let save = newCity.save()
+    save.then(function(city) {
+        res.send(`Saved ${city.name} as new city.`)
+    })
+})
+
+router.delete('/city/:cityName', function(req, res) {
+    //FIND CITY DATA IN DB AND REMOVE IT FROM DB
 })
 
 
