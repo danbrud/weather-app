2444ad60cb5bea8e5efa4ed72f619d5dccc73730
Danny <dannybrudner@gmail.com>
2019-04-14 09:06:58 -0400

    Added finishing touches

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,39 +12,44 @@ router.get('/sanity', function (req, res) {
 })
 
 
-router.get('/city/:cityName', function(req, res) {
+router.get('/city/:cityName', function (req, res) {
 
     //ROUTE THAT MAKES CALLS TO API
 
     let cityName = req.params.cityName
-    request(`https://api.apixu.com/v1/current.json?key=${API_KEY}&q=${cityName}`, function(err, response) {
+    request(`https://api.apixu.com/v1/current.json?key=${API_KEY}&q=${cityName}`, function (err, response) {
         let weatherObj = JSON.parse(response.body)
 
-        let lastUpdate = moment(weatherObj.current.last_updated, "YYYY-MM-DD hh-mm").format("ddd, h:mm A")
-       
-        let cityObj = {
-            name: weatherObj.location.name,
-            updatedAt: lastUpdate,
-            temperature: weatherObj.current.temp_c,
-            condition: weatherObj.current.condition.text,
-            conditionPic: weatherObj.current.condition.icon
+        if (weatherObj.error) {
+            return res.end()
+        } else {
+            let lastUpdate = moment(weatherObj.current.last_updated, "YYYY-MM-DD hh-mm").format("ddd, h:mm A")
+
+            let cityObj = {
+                name: weatherObj.location.name,
+                updatedAt: lastUpdate,
+                temperature: weatherObj.current.temp_c,
+                condition: weatherObj.current.condition.text,
+                conditionPic: weatherObj.current.condition.icon
+            }
+
+            res.send(cityObj)
         }
 
-        res.send(cityObj)
     })
 })
 
-router.get('/cities', function(req, res) {
+router.get('/cities', function (req, res) {
     //ROUTE THAT FINDS ALL CITY DATA SAVED IN DB AND SENDS IT TO CLIENT
-    City.find({}, function(err, cities) {
+    City.find({}, function (err, cities) {
         res.send(cities)
     })
 })
 
-router.post('/city', function(req, res) {
+router.post('/city', function (req, res) {
     //TAKE DATA FROM BODY OF THE REQUEST AND SAVE AS A NEW CITY TO DATABASE
     let reqCity = req.body
-    
+
     let newCity = new City({
         name: reqCity.name,
         updatedAt: reqCity.updatedAt,
@@ -53,25 +58,25 @@ router.post('/city', function(req, res) {
         conditionPic: reqCity.conditionPic
     })
 
-    
+
     let save = newCity.save()
-    save.then(function(city) {
+    save.then(function (city) {
         res.send(`Saved ${city.name} as new city.`)
     })
 })
 
-router.delete('/city/:cityName', function(req, res) {
+router.delete('/city/:cityName', function (req, res) {
     //FIND CITY DATA IN DB AND REMOVE IT FROM DB
     let cityName = req.params.cityName
-    City.deleteOne({ name: cityName }, function(err, response) {
+    City.deleteOne({ name: cityName }, function (err, response) {
         res.send(`Deleted ${cityName} from database`)
     })
 })
 
-router.put('/city/:cityName', async function(req, res) {
+router.put('/city/:cityName', async function (req, res) {
     let cityName = req.params.cityName
 
-    request(`https://api.apixu.com/v1/current.json?key=${API_KEY}&q=${cityName}`, async function(err, response) {
+    request(`https://api.apixu.com/v1/current.json?key=${API_KEY}&q=${cityName}`, async function (err, response) {
         let weatherObj = JSON.parse(response.body)
         let lastUpdate = moment(weatherObj.current.last_updated, "YYYY-MM-DD hh-mm").format("ddd, h:mm A")
 
@@ -83,7 +88,7 @@ router.put('/city/:cityName', async function(req, res) {
             conditionPic: weatherObj.current.condition.icon
         })
 
-        await City.deleteOne({ name: cityName })     
+        await City.deleteOne({ name: cityName })
         await newCity.save()
 
         res.send(newCity)
